bfd8eac663be6b190829b713fe01dacee0214ec8
enriquemoeller <enrique.moeller@onshoreoutsourcing.com>
2017-11-08 09:51:20 -0600

    Code cleanup
    Simplified code

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -16,37 +16,31 @@ namespace IntelligentVacuum.Agent
 
             bool isDirty = room.IsDirty;
 
-            List<int> moveSet = new List<int> { 0, 1, 2, 3 };
-            
-            Random rnd = new Random();
-
-            if (room.YAxis == 0)
-            {
-                moveSet.RemoveAt(0);
-            }
-            
-            if (room.XAxis == 0)
-            {
-                if (moveSet.Count > 3)
-                {
-                    moveSet.RemoveAt(2);
-                }
-                else
-                {
-                    moveSet.RemoveAt(1);
-                }
-            }
-
-            int index = rnd.Next(0, moveSet.Count);
-
-            int direction = moveSet[index];
-            
             if (isDirty)
             {
                 action = AgentAction.CLEAN;
             }
+
             else
             {
+                List<int> moveSet = new List<int> { 0, 1, 2, 3 };
+                
+                Random rnd = new Random();
+
+                if (room.YAxis == 0)
+                {
+                    moveSet.Remove(0);
+                }
+                
+                if (room.XAxis == 0)
+                {
+                    moveSet.Remove(2);
+                }
+
+                int index = rnd.Next(0, moveSet.Count);
+
+                int direction = moveSet[index];
+            
                 action = (AgentAction)direction;
             }
             return action;
